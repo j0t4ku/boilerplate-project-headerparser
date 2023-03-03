59986a76eaeada87dd918b697405107000c485d7
j0t4ku <j.ismar00@gmail.com>
2023-03-03 08:56:10 -0300

    Completed  Request Header Parser Project

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// api/whoami function 
+app.get('/api/whoami',(req,res)=>{
+  //get ip
+  const ip= req.ip;
+  //get language
+  const language= req.headers['accept-language'];
+  //get software
+  const software= req.headers['user-agent'];
+  res.send({ ipaddress : ip, language : language, software : software})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
